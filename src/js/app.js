a52260246732863ddf4d07395e0975dc8d6a11ce
Mariana Batista <mariana-batista99@hotmail.com>
2024-01-15 14:16:44 -0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ var projects = [
     imgSrc: './src/img/project6.avif',
     projectLink: 'https://jordan-shoes-ashy.vercel.app',
     title: 'Front-end',
-    description: 'Praticando html, css e js através do layout do Iuri Silda, da comunidade Codelândia',
+    description: 'Praticando html, css e js através do layout do Iuri Silva, da comunidade Codelândia',
   },
   {
     imgSrc: './src/img/project4.avif',
